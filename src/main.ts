7e2cd55530b08ba0b46b94a99684a2c22592cc96
Daniel Sarfati <danielrsarfati@gmail.com>
2024-04-15 15:18:25 -0600

    Update SCE APIs

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
 
 /**
  * The main function for the action.
@@ -7,18 +6,26 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    core.warning('Starting action')
+    let org = core.getInput('salad-organization')
+    let proj = core.getInput('salad-project')
+    let containerGroup = core.getInput('salad-container-group')
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    core.warning('Org:' + org)
+    core.warning('Project:' + proj)
+    core.warning('ContainerGroup:' + containerGroup)
+    // const ms: string = core.getInput('milliseconds')
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
+    // core.debug(`Waiting ${ms} milliseconds ...`)
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    // // Log the current timestamp, wait, then log the new timestamp
+    // core.debug(new Date().toTimeString())
+    // await wait(parseInt(ms, 10))
+    // core.debug(new Date().toTimeString())
+
+    // // Set outputs for other workflow steps to use
+    // core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
